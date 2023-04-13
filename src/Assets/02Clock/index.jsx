f948e1a46feedd3f7efe518efc94cbc23c9c8b1c
yichennn <yichennn1010@gmail.com>
2023-04-14 01:36:42 +0800

    add asset files

---
+++ src/Assets/02Clock/index.jsx
@@ -0,0 +1,6 @@
+import taiwan from './taiwan.jpg';
+import london from './london.jpg';
+import japan from './japan.jpg';
+import defaultBg from './default.jpg';
+
+export { taiwan, london, japan, defaultBg };
