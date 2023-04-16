8e81c0f6860a7505c8e1970bb75247db5d91c883
yichennn <yichennn1010@gmail.com>
2023-04-16 23:35:18 +0800

    feat: unit 6 ajax type

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -3,3 +3,4 @@ export { Clock } from './02Clock';
 export { ColorInput, RangeInput } from './03Input';
 export { StyledTable } from './04Table';
 export { Panel } from './05ImagePanel';
+export { ListItem } from './06ListItem';
