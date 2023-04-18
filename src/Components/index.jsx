358a2699830566f47ea80000574820a345f5a477
yichennn <yichennn1010@gmail.com>
2023-04-18 22:53:19 +0800

    feat: unit 8 canvas base painting

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -4,3 +4,4 @@ export { ColorInput, RangeInput } from './03Input';
 export { StyledTable } from './04Table';
 export { Panel } from './05ImagePanel';
 export { ListItem } from './06ListItem';
+export { CanvasArea } from './08Canvas';
--- src/Pages/08Canvas/index.jsx
+++ src/Pages/08Canvas/index.jsx
@@ -1,12 +1,17 @@
+import { CanvasArea } from 'Components';
 import styles from './styles.module.scss';
 
 export const CanvasPage = () => {
-  console.log('08 Page');
+  const title = 'Start painting what you want!';
 
   return (
     <section className={styles.page}>
       <div className={styles.container}>
-        <h2>08 Page</h2>
+        <h2 className={styles.title}>
+          {title}
+          <div>{title}</div>
+        </h2>
+        <CanvasArea />
       </div>
     </section>
   );
