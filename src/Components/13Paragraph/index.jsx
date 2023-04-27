61227a5c6fa5e578abf9a4a9ab37ca53029cf030
yichennn <yichennn1010@gmail.com>
2023-04-28 00:48:54 +0800

    feat: add scroll animate on each Paragraphy component

--- src/Components/13Paragraph/index.jsx
+++ src/Components/13Paragraph/index.jsx
@@ -1,13 +1,25 @@
+import { useRef } from 'react';
+import { useOnScreen } from './useOnScreen';
 import styles from './styles.module.scss';
 
 export const Paragraph = ({ text, image, imagePosition = 'L' }) => {
-  const className = imagePosition === 'R' ? styles.float_right : styles.float_left;
+  const ref = useRef();
+  const onScreen = image && useOnScreen(ref);
+  const floatClassName = imagePosition === 'R' ? styles.float_right : styles.float_left;
+  const imgClassname =
+    imagePosition === 'R'
+      ? onScreen
+        ? styles.image_right_show
+        : styles.image_right_hide
+      : onScreen
+      ? styles.image_left_show
+      : styles.image_left_hide;
 
   return (
     <div className={styles.paragraph_container}>
       {image && (
-        <div className={className}>
-          <img src={image} alt='...' />
+        <div className={floatClassName} ref={ref}>
+          <img src={image} alt='...' className={imgClassname} />
         </div>
       )}
       <div className={styles.text}>{text}</div>
