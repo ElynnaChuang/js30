a7c3fea9b4d74fd63bf894a3e318b5d767c071fe
yichennn <yichennn1010@gmail.com>
2023-04-27 19:13:48 +0800

    feat: unit 12 key detection

--- src/Pages/12KeyDetection/index.jsx
+++ src/Pages/12KeyDetection/index.jsx
@@ -1,12 +1,69 @@
+import { useRef, useEffect } from 'react';
+import JSConfetti from 'js-confetti';
 import styles from './styles.module.scss';
+import confettiSetting from './confettiSetting';
+
+const SECRET_CODE = 'yay';
+let jsConfetti;
 
 export const KeyDetectionPage = () => {
-  console.log('12 Page');
+  const canvasRef = useRef(null);
+  const pressed = [];
+
+  const generateConfetti = e => {
+    e.stopPropagation();
+
+    const randomNum = Math.floor(Math.random() * confettiSetting.length);
+    const setting = confettiSetting[randomNum];
+
+    jsConfetti.addConfetti(setting);
+  };
+
+  const handleKeyUp = e => {
+    e.stopPropagation();
+    const locationArr = window.location.pathname.split('/');
+    const url = locationArr[locationArr.length - 1];
+    if (url !== '12') return;
+
+    const { key } = e;
+    pressed.push(key);
+    pressed.splice(0, pressed.length - SECRET_CODE.length); // pressed.length 永遠不會超過 SECRET_CODE.length
+    if (pressed.join('').includes(SECRET_CODE)) {
+      generateConfetti(e);
+    }
+  };
+
+  useEffect(() => {
+    if (canvasRef.current) jsConfetti = new JSConfetti({ canvasRef });
+    window.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      if (jsConfetti) jsConfetti.clearCanvas();
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
 
   return (
     <section className={styles.page}>
-      <div className={styles.container}>
-        <h2 className={styles.title}>12 Page</h2>
+      <div className={styles.info}>
+        <p>You can</p>
+        <div>
+          <p>Enter</p>
+          <span>{SECRET_CODE}</span>
+          <p>or Click</p>
+          <button className={styles.button} onClick={generateConfetti}>
+            Button
+          </button>
+        </div>
+        <p>to get happiness!</p>
+      </div>
+      <div className={styles.canvas_container}>
+        <canvas
+          ref={canvasRef}
+          height='100%'
+          className={styles.canvas}
+          // className={animateStart ? styles.canvas_show : styles.canvas_hide}
+        />
       </div>
     </section>
   );
