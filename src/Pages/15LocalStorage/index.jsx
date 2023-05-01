bcd22ee0b30e1d89960335e33641013195a0a931
yichennn <yichennn1010@gmail.com>
2023-05-01 16:12:26 +0800

    feat: create new item & done & page style

--- src/Pages/15LocalStorage/index.jsx
+++ src/Pages/15LocalStorage/index.jsx
@@ -1,10 +1,33 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { FishIcon } from '@/Assets/09Icons';
+import { Form, List } from '@/Components';
 import styles from './styles.module.scss';
 
 export const LocalStoragePage = () => {
+  const [listItems, setListItems] = useState([]);
+
+  const handleAddItem = value => {
+    if (!value.trim().length) return;
+
+    const id = uuidv4();
+    setListItems(prev => [...prev, { id, name: value, done: false }]);
+  };
+
+  const handleDone = id => {
+    setListItems(prev => prev.map(el => (el.id === id ? { ...el, done: !el.done } : el)));
+  };
+
   return (
     <section className={styles.page}>
       <div className={styles.container}>
-        <h2 className={styles.title}>15</h2>
+        <FishIcon />
+
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>LOCAL TAPAS</h2>
+          <Form addItem={handleAddItem} />
+          <List items={listItems} handleDone={handleDone} />
+        </div>
       </div>
     </section>
   );
