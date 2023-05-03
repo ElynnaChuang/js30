5cb20d1aa4efedb5d4fcc272a3c05af9a91c24b7
yichennn <yichennn1010@gmail.com>
2023-05-03 15:19:05 +0800

    adjust: 01 bg

--- src/Components/Image/index.jsx
+++ src/Components/Image/index.jsx
@@ -2,44 +2,46 @@ import { useInView } from 'react-intersection-observer';
 import phJpg from '@/Assets/Homepage/placeholder.jpg';
 import styles from './styles.module.scss';
 
-export const Image = ({ images }) => {
+export const Image = ({ images, className }) => {
   const { s1x, s2x, s3x, m1x, m2x, m3x, l1x, l2x, l3x, base } = images;
   const { ref, inView } = useInView({
     triggerOnce: true,
   });
 
   return (
-    <picture>
-      {(l1x || l2x || l3x) && (
-        <source
-          media='(min-width: 992px)'
-          srcSet={inView ? `${l1x} 1x, ${l2x} 2x, ${l3x} 3x` : ''}
-          type='image/webp'
-        />
-      )}
+    <div className={`${styles.img_container} ${className}`}>
+      <picture>
+        {(l1x || l2x || l3x) && (
+          <source
+            media='(min-width: 992px)'
+            srcSet={inView ? `${l1x} 1x, ${l2x} 2x, ${l3x} 3x` : ''}
+            type='image/webp'
+          />
+        )}
 
-      {(m1x || m2x || m3x) && (
-        <source
-          media='(max-width: 991px)'
-          srcSet={inView ? `${m1x} 1x, ${m2x} 2x, ${m3x} 3x` : ''}
-          type='image/webp'
-        />
-      )}
+        {(m1x || m2x || m3x) && (
+          <source
+            media='(max-width: 991px)'
+            srcSet={inView ? `${m1x} 1x, ${m2x} 2x, ${m3x} 3x` : ''}
+            type='image/webp'
+          />
+        )}
 
-      {(s1x || s2x || s3x) && (
-        <source
-          media='(max-width: 575px)'
-          srcSet={inView ? `${s1x} 1x, ${s2x} 2x, ${s3x} 3x` : ''}
-          type='image/webp'
+        {(s1x || s2x || s3x) && (
+          <source
+            media='(max-width: 575px)'
+            srcSet={inView ? `${s1x} 1x, ${s2x} 2x, ${s3x} 3x` : ''}
+            type='image/webp'
+          />
+        )}
+        <img
+          ref={ref}
+          src={inView ? base : phJpg}
+          alt='...'
+          loading='lazy'
+          className={styles.img}
         />
-      )}
-      <img
-        ref={ref}
-        src={inView ? base : phJpg}
-        alt='...'
-        loading='lazy'
-        className={styles.img}
-      />
-    </picture>
+      </picture>
+    </div>
   );
 };
