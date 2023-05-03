4c1b54bb0f961b1fd7a47b991d98a3439c8b1fbc
yichennn <yichennn1010@gmail.com>
2023-05-03 19:57:07 +0800

    adjust: home page base image

--- src/Assets/Homepage/base/index.jsx
+++ src/Assets/Homepage/base/index.jsx
@@ -1,32 +1,63 @@
-import base01 from './01.png';
-import base02 from './02.png';
-import base03 from './03.png';
-import base04 from './04.png';
-import base05 from './05.png';
-import base06 from './06.png';
-import base07 from './07.png';
-import base08 from './08.png';
-import base09 from './09.png';
-import base10 from './10.png';
-import base11 from './11.png';
-import base12 from './12.png';
-import base13 from './13.png';
-import base14 from './14.png';
-import base15 from './15.png';
-import base16 from './16.png';
-import base17 from './17.png';
-import base18 from './18.png';
-import base19 from './19.png';
-import base20 from './20.png';
-import base21 from './21.png';
-import base22 from './22.png';
-import base23 from './23.png';
-import base24 from './24.png';
-import base25 from './25.png';
-import base26 from './26.png';
-import base27 from './27.png';
-import base28 from './28.png';
-import base29 from './29.png';
-import base30 from './30.png';
+import base01 from './01.jpg';
+import base02 from './02.jpg';
+import base03 from './03.jpg';
+import base04 from './04.jpg';
+import base05 from './05.jpg';
+import base06 from './06.jpg';
+import base07 from './07.jpg';
+import base08 from './08.jpg';
+import base09 from './09.jpg';
+import base10 from './10.jpg';
+import base11 from './11.jpg';
+import base12 from './12.jpg';
+import base13 from './13.jpg';
+import base14 from './14.jpg';
+import base15 from './15.jpg';
+import base16 from './16.jpg';
+import base17 from './17.jpg';
+import base18 from './18.jpg';
+import base19 from './19.jpg';
+import base20 from './20.jpg';
+import base21 from './21.jpg';
+import base22 from './22.jpg';
+import base23 from './23.jpg';
+import base24 from './24.jpg';
+import base25 from './25.jpg';
+import base26 from './26.jpg';
+import base27 from './27.jpg';
+import base28 from './28.jpg';
+import base29 from './29.jpg';
+import base30 from './30.jpg';
 
-export { base01, base02, base03, base04, base05, base06, base07, base08, base09, base10, base11, base12, base13, base14, base15, base16, base17, base18, base19, base20, base21, base22, base23, base24, base25, base26, base27, base28, base29, base30 };
+export {
+  base01,
+  base02,
+  base03,
+  base04,
+  base05,
+  base06,
+  base07,
+  base08,
+  base09,
+  base10,
+  base11,
+  base12,
+  base13,
+  base14,
+  base15,
+  base16,
+  base17,
+  base18,
+  base19,
+  base20,
+  base21,
+  base22,
+  base23,
+  base24,
+  base25,
+  base26,
+  base27,
+  base28,
+  base29,
+  base30,
+};
