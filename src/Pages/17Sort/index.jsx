34202d1769787dd477688fa8727f3d7f4354bd19
yichennn <yichennn1010@gmail.com>
2023-05-04 22:42:40 +0800

    feat: page 17 style

--- src/Pages/17Sort/index.jsx
+++ src/Pages/17Sort/index.jsx
@@ -1,13 +1,39 @@
 import styles from './styles.module.scss';
+import { itemsData } from './data';
+import { Image } from '@/Components';
+import { images } from '@/Assets/17bg';
 
 const SortPage = () => {
   return (
     <section className={styles.page}>
+      <Image images={images} className={styles.bg} />
       <div className={styles.container}>
-        <h2 className={styles.title}>17 Page</h2>
+        <div className={styles.title}>
+          <h1>Sorted Bands</h1>
+          <p>sorting by name without articles</p>
+        </div>
+        <List items={itemsData} />
       </div>
     </section>
   );
 };
 
 export default SortPage;
+
+export const List = ({ items = [] }) => {
+  return (
+    <ul className={styles.list}>
+      {items.map(({ id, name }) => (
+        <ListItem key={id} id={id} name={name} />
+      ))}
+    </ul>
+  );
+};
+
+const ListItem = ({ id, name }) => {
+  return (
+    <li className={styles.item} id={id}>
+      {name}
+    </li>
+  );
+};
