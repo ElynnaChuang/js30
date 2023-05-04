373f06a6d02ef197e13b031fe1547a4895c52238
yichennn <yichennn1010@gmail.com>
2023-05-04 18:12:32 +0800

    feat: unit 16 base page

---
+++ src/Pages/16MouseMove/index.jsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.scss';
+
+const MouseMovePage = () => {
+  return (
+    <section className={styles.page}>
+      <div className={styles.container}>
+        <h1>🔥WOAH!</h1>
+      </div>
+    </section>
+  );
+};
+
+export default MouseMovePage;
