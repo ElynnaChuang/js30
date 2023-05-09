115637132520a7cdecf07f4922979c9686643291
yichennn <yichennn1010@gmail.com>
2023-05-10 01:30:22 +0800

    feat: unit 21 base page

---
+++ src/Pages/21Geolocation/index.jsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss';
+
+const GeolocationPage = () => {
+  return (
+    <section className={styles.page}>
+      <div className={styles.container} />
+    </section>
+  );
+};
+
+export default GeolocationPage;
