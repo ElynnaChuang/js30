c53cec06c3f277529ffe07c217fd74f8825a690a
yichennn <yichennn1010@gmail.com>
2023-05-16 18:26:30 +0800

    feat: page style

---
+++ src/Pages/24StickyNav/data.jsx
@@ -0,0 +1,33 @@
+const bannerImages = [
+  {
+    url: 'https://source.unsplash.com/nQ94b_bn08o',
+    caption: 'Banner 1',
+  },
+  {
+    url: 'https://source.unsplash.com/nKzeG3iE_Qg',
+    caption: 'Banner 2',
+  },
+
+  {
+    url: 'https://source.unsplash.com/46TvM-BVrRI',
+    caption: 'Banner 3',
+  },
+];
+
+const paragraphImages = [
+  {
+    key: 1,
+    url: 'https://source.unsplash.com/HoC9ttceIGo',
+  },
+  {
+    key: 2,
+    url: 'https://source.unsplash.com/bIW1a6svo1k',
+  },
+
+  {
+    key: 3,
+    url: 'https://source.unsplash.com/SlZq1f2tmaM',
+  },
+];
+
+export { bannerImages, paragraphImages };
